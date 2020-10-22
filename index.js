487a732b391e6fd27a979d31df2be3f395190bcd
片岡駿介 <sk0413.jp@gmail.com>
2020-10-22 23:55:34 +0900

    debug

--- index.js
+++ index.js
@@ -68,49 +68,49 @@ const handleMessageEvent = async (ev) => {
     console.log(profile);
   });
   //返事を送信
-  if (text === '聞いて') {
-    return client.replyMessage(ev.replyToken, {
-      type: "text",
-      text: `どうしました？`,
-      values: 'what'
-    });
-  } else if (splitData[0] === 'what') {
-    const splitData = data.split('&');
-    const Whatareudoing = splitData[1]
-    askfeel(ev, Whatareudoing)
-  } else if (splitData[0] === 'feel') {
-    const Whatareudoing = splitData[1]
-    const Howdoufeel = splitData[2]
-    return client.replyMessage(ev.replyToken, {
-      type: "text",
-      text: `どうしてそう感じたのですか？`,
-      values: `why&${Whatareudoing}&${Howdoufeel}`
-    });
-  } else if (splitData[0] === 'why') {
-    const Whatareudoing = splitData[1]
-    const Howdoufeel = splitData[2]
-    const whyareufeelso = splitData[3]
-    return client.replyMessage(ev.replyToken, {
-      type: "text",
-      text: `そこから何を学びましたか？`,
-      values: `learn&${Whatareudoing}&${Howdoufeel, whyareufeelso}`
-    });
-  } else if (splitData[0] === 'learn') {
-    const Whatareudoing = splitData[1]
-    const Howdoufeel = splitData[2]
-    const whyareufeelso = splitData[3]
-    const whatdoulearn = splitData[4]
-    return client.replyMessage(ev.replyToken, {
-      type: "text",
-      text: `そこから何を学びましたか？`,
-      values: `learn&${Whatareudoing}&${Howdoufeel, whyareufeelso}`
-    });
-  } else {
-    return client.replyMessage(ev.replyToken, {
-      type: "text",
-      text: `${profile.displayName}さん、今「${ev.message.text}」って言いました？`
-    });
-  }
+  // if (text === '聞いて') {
+  //   return client.replyMessage(ev.replyToken, {
+  //     type: "text",
+  //     text: `どうしました？`,
+  //     values: 'what'
+  //   });
+  // } else if (splitData[0] === 'what') {
+  //   const splitData = data.split('&');
+  //   const Whatareudoing = splitData[1]
+  //   askfeel(ev, Whatareudoing)
+  // } else if (splitData[0] === 'feel') {
+  //   const Whatareudoing = splitData[1]
+  //   const Howdoufeel = splitData[2]
+  //   return client.replyMessage(ev.replyToken, {
+  //     type: "text",
+  //     text: `どうしてそう感じたのですか？`,
+  //     values: `why&${Whatareudoing}&${Howdoufeel}`
+  //   });
+  // } else if (splitData[0] === 'why') {
+  //   const Whatareudoing = splitData[1]
+  //   const Howdoufeel = splitData[2]
+  //   const whyareufeelso = splitData[3]
+  //   return client.replyMessage(ev.replyToken, {
+  //     type: "text",
+  //     text: `そこから何を学びましたか？`,
+  //     values: `learn&${Whatareudoing}&${Howdoufeel, whyareufeelso}`
+  //   });
+  // } else if (splitData[0] === 'learn') {
+  //   const Whatareudoing = splitData[1]
+  //   const Howdoufeel = splitData[2]
+  //   const whyareufeelso = splitData[3]
+  //   const whatdoulearn = splitData[4]
+  //   return client.replyMessage(ev.replyToken, {
+  //     type: "text",
+  //     text: `そこから何を学びましたか？`,
+  //     values: `learn&${Whatareudoing}&${Howdoufeel, whyareufeelso}`
+  //   });
+  //} else {
+  return client.replyMessage(ev.replyToken, {
+    type: "text",
+    text: `${profile.displayName}さん、今「${ev.message.text}」って言いました？`
+  });
+  //}
 }
 
 
